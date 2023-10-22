905496ce0ff4b4236a749eb29ba1603ebff84423
Harshvardhan Amreliya <Hamreliya04@gmail.com>
2023-10-22 18:07:48 +0530

    add splitting equally functionality

--- frontend/message-request.js
+++ frontend/message-request.js
@@ -5,10 +5,20 @@ const btn_split_equally = document.querySelector('.split-equally-btn');
 const btn_split_by_amount = document.querySelector('.split-by-amount-btn');
 const div_split_equally = document.querySelector('.request-split-equally');
 const div_split_by_amount = document.querySelector('.request-split-by-amount');
-const span_request_main_body_splitting_option_btn = document.querySelector('.request-main-body-splitting-option-btn')
+const span_request_main_body_splitting_option_btn = document.querySelector('.request-main-body-splitting-option-btn');
+const input_split_equally = document.querySelector('.request-main-body-hero-form-cost');
+const input_split_unequally = document.querySelector('.request-split-by-amount-input');
 
 splitting_modal_popup.addEventListener('click', function() {
-    splitting_option_modal_popup.classList.toggle('hidden');
+    const amount = +input_split_equally.value;
+    const members = Array.from(document.querySelectorAll('.request-split-equally-amount'));
+    const amount_to_split_equally = (amount / (members.length)).toFixed(2);
+    members.forEach(output => {
+        output.textContent = amount_to_split_equally;
+    })
+    if(splitting_option_modal_popup.classList.contains('hidden')){
+        splitting_option_modal_popup.classList.remove('hidden');
+    }
 })
 
 dismiss.addEventListener('click',function() {
@@ -26,3 +36,16 @@ btn_split_by_amount.addEventListener('click', () =>{
     div_split_by_amount.style.display = 'flex';
     span_request_main_body_splitting_option_btn.innerHTML = 'unequally';
 })
+
+const invalidChars = [
+    "-",
+    "+",
+    "e",
+    "E"
+  ];
+
+  [input_split_equally,input_split_unequally].forEach(input=>{input.addEventListener("keypress", function(e) {
+    if (invalidChars.includes(e.key)) {
+      e.preventDefault();
+    }
+  })})
